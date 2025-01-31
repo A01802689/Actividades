77b110cc9248694d74ea125f5bdea6cf908dd9a3
A01802689 <A01802689@tec.mx>
2025-01-31 01:18:13 +0000

    Avance10.01

---
+++ SistemaReservacion.h
@@ -0,0 +1,15 @@
+#ifndef RESERVACION_H
+#define RESERVACION_H
+#include <vector>
+#include "Vuelo.h"
+
+using namespace std;
+
+class Historial{
+
+    private:
+
+    vector<Vuelo> ReservaAvion;
+
+};
+#endif
